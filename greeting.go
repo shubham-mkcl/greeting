95976ea090bf41b548d4aba54c5d7bf5663551f2
shubamkumar <shubhamjaiswal20111998@gmail.com>
2021-08-13 11:49:18 +0530

    Initial commit

---
+++ greeting.go
@@ -0,0 +1,15 @@
+package greeting
+
+import "fmt"
+
+func Hello() {
+	fmt.Println("Hello GO World!!!")
+}
+
+func Hii() {
+	fmt.Println("Hii Go!!")
+}
+
+func HelloFromShubham() {
+	fmt.Println("Hello from Shubham")
+}
